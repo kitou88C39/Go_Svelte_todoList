b036ce31a71175be13827a7138244cb8177e0509
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-31 20:29:41 +0900

    main.goを作成する

--- main.go
+++ main.go
@@ -6,11 +6,5 @@ import (
 
 func main(){
 	fmt.Println("Hello world")
-	var myName string = "john Doe"
-	const mySecondName string = "john Doe"
-
-	myThirdName := "Bob Doe"
-	fmt.Println(myName)
-	fmt.Println(mySecondName)
-	fmt.Println(myThirdName)
+	
 }
