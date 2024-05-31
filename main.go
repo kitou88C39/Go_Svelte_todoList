755a2365a8612c94c24137b528db502b191755e7
kitoumasaomi <kanfutrooper@gmail.com>
2024-05-31 20:18:16 +0900

    main.goを作成する

--- main.go
+++ main.go
@@ -1,25 +1,11 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"fmt"
 )
 
 func main(){
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Use(JSONMiddleware)
-	r.Get("/",func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello world"))
-	})
-	http.ListenAndServe("localhost:9000",r)
-}
-
-func JSONMiddleware(next http.Handler) http.Handler{
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		next.ServeHTTP(w,r)
-	})
+	fmt.Println("Hello world")
+	var myName string = "john Doe"
+	const mySecondName string = "john Doe"
 }
