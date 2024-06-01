2e3497a044847d1450280dbae6829b9b1c760231
kitoumasaomi <kanfutrooper@gmail.com>
2024-06-01 11:29:04 +0900

    　main.goを作成する

--- main.go
+++ main.go
@@ -19,8 +19,8 @@ func main(){
 
 	todos := []Todo{}
 
-	app.Get("/", func (c *fiber.Ctx) error {
-		return c.Status(200).JSON(fiber.Map{"msg":"hello world"})
+	app.Get("/api/todos", func (c *fiber.Ctx) error {
+		return c.Status(200).JSON(todos)
 	})
 
 	app.Post("/api/todos", func(c *fiber.Ctx) error {
@@ -53,6 +53,8 @@ app.Patch("/api/todos/:id",func(c *fiber.Ctx) error {
 	}
 	return c.Status(404).JSON(fiber.Map{"error":"Todo not found"})
 })
-
+//Delete a Todo
+app.Patch("/api/todos/:id")
+	
 	log.Fatal(app.Listen(":4000"))
 }
